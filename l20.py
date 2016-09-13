6fa1a08c0d5ea7613b8326fea3efc4438c82f587
James Parkin <jparkin@caltech.edu>
2016-09-13 11:28:30 -0700

    lessons 18-20

--- l20.py
+++ l20.py
@@ -1,12 +1,14 @@
 import numpy as np
+
 xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
 
 def xa_to_diameter(xa):
     '''
-    Convert an array of cross-sectional ares
+    Convert an array of cross-sectional areas
+    to diameters with commensurate units
     '''
-    diameter = np.sqrt(xa*4/np.pi)
-    return diameter
 
-diams = xa_to_diameter(xa_high)
+    # Compute diameter from area
+    # A = pi * d^2 / 4
+    diameter = np.sqrt(xa *4 / np.pi)
